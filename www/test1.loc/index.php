dc22f2079b6c0f7a84c6bfad7a04af5d6124cc20
Igor <31731246+fzsys@users.noreply.github.com>
2020-02-22 10:37:21 +0200

    updated test1.loc index page

--- www/test1.loc/index.php
+++ www/test1.loc/index.php
@@ -1,8 +1,8 @@
 <?php
-echo 'virtual host test1.loc is ready to handle requests<br>';
 
 if ($link = mysqli_connect('mysql', 'admin', 'adminpass', 'test')) {
-	echo 'database connection established';
+	echo 'database connection established<br>';
+	echo 'virtual host test1.loc is ready to handle requests';
 } else {
 	echo 'database connection error';
 }
